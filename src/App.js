b080cb7f93b919980550ad38ef322e0ba19e2947
fajar241101 <mf.fajarramadhan.sh@gmail.com>
2023-03-09 16:32:01 +0700

    website pertamaku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import Maincontent from './components/Maincontents';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <div>
+    <Navigation />
+    <Maincontent />
+    </div>   
+      );
+    }
+    export default App;
