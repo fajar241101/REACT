82ea9f0dca3f3a2bc86a9e6b45c11c80320cdf17
fajar241101 <mf.fajarramadhan.sh@gmail.com>
2023-03-13 10:15:26 +0700

    fajar dreamsite

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import Maincontent from './components/Maincontents';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
     <Navigation />
     <Maincontent />
+    <Footer />
     </div>   
       );
     }
